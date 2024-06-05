f5e9f0a8b55c05ece9e30d1c542fc9793848f53d
Manon Carbonnel <manon.carbonnel@gmail.com>
2024-06-05 14:11:05 +0200

    Add property based testing
    Co-authored-by: Mathieu Lirzin <mthl@reuz.fr>

--- test/fizzbuzz/core_test.clj
+++ test/fizzbuzz/core_test.clj
@@ -1,8 +1,22 @@
 (ns fizzbuzz.core-test
   (:require
    [clojure.test :refer [deftest is testing]]
+   [clojure.test.check.clojure-test :refer [defspec]]
+   [clojure.test.check.generators :as gen]
+   [clojure.test.check.properties :as prop]
    [fizzbuzz.core :as sut]
    [fizzbuzz.constant :as c]))
+
+(defspec fizzbuzz-spec
+  (prop/for-all [n gen/nat] 
+   (or (= 0 n)
+       (let [r (sut/fizzbuzz n)]
+         (and (string? r)
+              (cond
+                (= 0 (mod n 15)) (= c/fizzbuzz r)
+                (= 0 (mod n 3)) (= c/fizz r)
+                (= 0 (mod n 5)) (= c/buzz r)
+                :else (= (str n) r)))))))
 
 (deftest fizzbuzz-test
   (testing "Default behaviour with numbers"
